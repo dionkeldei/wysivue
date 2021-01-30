2240d0cd2ff13fbc0f55d1784343b1c27c626c70
Dionkeldei <dionkeldei@gmail.com>
2021-01-29 18:29:41 -0600

    wysiwig working

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
   <div id="wysivue">
+    <ckeditor :editor="editor" v-model="value" :config="editorConfig"></ckeditor>
   </div>
 </template>
 
 <script>
-
+import CKEditor from '@ckeditor/ckeditor5-vue2'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 export default {
   name: 'Wysivue',
+  data () {
+    return {
+      editor: ClassicEditor,
+      value: '',
+      editorConfig: {
+        toolbar: {
+          items: [
+            'heading',
+            'bold',
+            'italic',
+            'link',
+            'BulletedList',
+            'NumberedList',
+            'undo',
+            'redo'
+          ]
+        }
+      }
+    }
+  },
+  mounted () {
+    this.value = this.html
+  },
+  props: {
+    html: String
+  },
   components: {
+    ckeditor: CKEditor.component
+  },
+  watch: {
+    value: function (value) {
+      this.$emit('change', value)
+    },
+    html: function () {
+      this.value = this.html
+    }
   }
 }
 </script>
